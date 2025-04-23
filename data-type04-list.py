35e8ecd41da6b11cc6872089b4be2a4ac37bab0a
MohammadAli <mohammad.work0076@gmail.com>
2025-04-23 12:37:45 +0330

    talk about list

---
+++ data-type04-list.py
@@ -0,0 +1,19 @@
+# List Data Type in Python
+
+# List declaration
+nums = [1, 2, 3, 4, 5]
+mixed = [1, "hello", 3.14, True]
+empty = []
+
+# Accessing elements
+print(nums[0])    # 1
+print(nums[-1])   # 5
+print(nums[1:4])  # [2, 3, 4]
+
+# Modifying elements
+nums[0] = 100
+print(nums)       # [100, 2, 3, 4, 5]
+
+# List methods
+nums.append(6)             # Add to end
+nums.insert(1, 200
